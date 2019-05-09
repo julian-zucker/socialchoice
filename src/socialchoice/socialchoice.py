3237380de0f71d803c251a2067794cb69b64d28b
Julian Zucker <julian.zucker@gmail.com>
2019-05-09 11:00:23 -0400

    documentation and consistent naming for election
    * remove getter and post-construction adding of votes
    * rename all ranking methods to ranking_by
    * add documentation for all non-ranking methods
    
    Authored-by: Julian Zucker <julian.zucker@gmail.com>

--- src/socialchoice/socialchoice.py
+++ src/socialchoice/socialchoice.py
@@ -1,34 +1,30 @@
 import networkx
 
+
 class Election:
     """
     A user adds votes to the election and can retrieve results from the election.
     """
 
-    def __init__(self, *votes):
+    def __init__(self, votes):
         """
         :param votes: An array of votes, where a vote is any indexable object of length 3
                 Each vote looks like this: ["Alice",  "Bob", "won"] which indicates that "Alice" lost to "Bob".
         """
-        assert are_valid_votes(votes)
+        assert self.__are_valid_votes(votes)
         self._votes = list(votes)
-        self._candidates = get_all_candidates_from_votes(self._votes)
-
-    def add_votes(self, *votes) -> None:
-        assert are_valid_votes(votes)
-        self._votes += votes
-        self._candidates |= get_all_candidates_from_votes(votes)
-
-    def get_votes(self) -> list:
-        return self._votes.copy()
+        self._candidates = self.__get_all_candidates_from_votes(self._votes)
 
     def get_candidates(self) -> set:
+        """:return: the set of candidates being voted on in this election"""
         return self._candidates.copy()
 
     def get_victory_graph(self) -> networkx.DiGraph:
-        """
-        :return: Every out-edge is a loss to another candidate, every in-edge is a win
-        """
+        """A victory graph is a matchup graph of each candidate, but with only edges for wins. Every out-edge represents
+         a win over another candidate.
+
+        See `get_matchup_graph` for a description of the attributes on nodes and edges.
+         """
         matchups = self.get_matchup_graph()
         edges_to_remove = []
         for u, v in matchups.edges:
@@ -43,8 +39,14 @@ class Election:
         return matchups
 
     def get_matchup_graph(self) -> networkx.DiGraph:
-        """
-        :return: fully connected graph
+        """A matchup graph is a fully-connected graph, with each out-edge corresponding to a matchup and each in-edge
+        corresponding to the same matchup, but with wins and losses flipped. An edge `(a, b)` from a to b will have
+        `wins` corresponding to the number of wins `a` has over `b`, while the edge `(b, a)` will have `wins`
+        corresponding to the number of wins `b` has over `a`.
+
+        Every edge has the attributes `wins` (described above), `losses`, `ties`, and `margin`. `losses` and `ties` are
+        self explanatory, simply the number of losses or ties between the two candidates. `margin` is the ratio of
+        wins to overall votes.
         """
         matchups = self.get_matchups()
         ids = matchups.keys()
@@ -73,12 +75,27 @@ class Election:
 
         return g
 
-
     def get_matchups(self) -> dict:
+        """This matchup shows the number of wins and losses each candidate has against each other.
+        The shape of the final dictionary returned is:
+
+         >>> {\
+            "candidate1": { \
+                 {"candidate2": {"win": 12, "loss": 10, "tie": 5}}, \
+                 {"candidate3": {"win": 3,  "loss": 23, "tie": 9}}, \
+                 # potentially many more \
+             }, \
+             "candidate2": { \
+                 {"candidate1": {"win": 10, "loss": 12, "tie": 5}}, \
+                 {"candidate3": {"win": 23, "loss":  3, "tie": 9}}, \
+                 # potentially many more \
+             },\
+             # and so on\
+          }
+
+        :return: a matchup mapping, as described above
         """
-        :return: Mapping from candidate to a mapping from the candidates they have played to their "win" "losses" "ties"
-        """
-        matchups = {} # A matrix so we can have all the
+        matchups = {}
         for candidate in self._candidates:
             matchups[candidate] = {}
         for candidate1 in self._candidates:
@@ -102,7 +119,7 @@ class Election:
 
         return matchups
 
-    def get_ranked_pairs_ranking(self) -> list:
+    def ranking_by_ranked_pairs(self) -> list:
         matchups = self.get_victory_graph()
 
         g = networkx.DiGraph()
@@ -122,34 +139,15 @@ class Election:
         return list(networkx.topological_sort(g))
 
 
-    def ranked_pairs(self) -> list:
-        matchups = self.get_victory_graph()
-
-        g = networkx.DiGraph()
-        g.add_nodes_from(matchups.nodes)
-
-        edges = [(u, v, matchups.get_edge_data(u, v)) for (u, v) in matchups.edges]
-        edges.sort(key=lambda x: x[2]["margin"], reverse=True)
-        for u, v, data in edges:
-            g.add_edge(u, v, **data)
-            try:
-                networkx.find_cycle(g)
-                g.remove_edge(u, v)
-            except networkx.NetworkXNoCycle:
-                pass
-
-        assert networkx.is_directed_acyclic_graph(g)
-        return list(networkx.topological_sort(g))
-
-    def get_copeland(self) -> list:
+    def ranking_by_copeland(self) -> list:
         g = self.get_victory_graph()
         return sorted([(n, g.out_degree(n) - g.in_degree(n)) for n in g.nodes], key=lambda x: x[1], reverse=True)
 
-    def get_minimax(self) -> list:
+    def ranking_by_minimax(self) -> list:
         g = self.get_matchup_graph()
         return sorted(g.nodes, key=lambda n: max(g.get_edge_data(u, v)["margin"] for u, v in g.in_edges(n)))
 
-    def get_win_ratio(self) -> list:
+    def ranking_by_win_ratio(self) -> list:
         matchups = self.get_matchups()
 
         wins_and_ties_vs_losses = {}
@@ -165,7 +163,7 @@ class Election:
                   for candidate, x in wins_and_ties_vs_losses.items()]
         return sorted(ratios, key=lambda x: x[1], reverse=True)
 
-    def get_win_tie_ratio(self) -> list:
+    def ranking_by_win_tie_ratio(self) -> list:
         matchups = self.get_matchups()
 
         wins_and_ties_vs_losses = {}
@@ -183,11 +181,13 @@ class Election:
         return sorted(ratios, key=lambda x: x[1], reverse=True)
 
 
-def get_all_candidates_from_votes(votes) -> set:
-    """
-    :return: All the candidates mentioned in the votes
-    """
-    return {vote[0] for vote in votes} | {vote[1] for vote in votes}
+    @staticmethod
+    def __get_all_candidates_from_votes(votes) -> set:
+        """
+        :return: All the candidates mentioned in the votes
+        """
+        return {vote[0] for vote in votes} | {vote[1] for vote in votes}
 
-def are_valid_votes(votes: iter) -> bool:
-    return all(vote[2] in {"win", "loss", "tie"} for vote in votes)
+    @staticmethod
+    def __are_valid_votes(votes: iter) -> bool:
+        return all(len(vote) == 3 and vote[2] in {"win", "loss", "tie"} for vote in votes)
--- test/test_election.py
+++ test/test_election.py
@@ -2,52 +2,29 @@ from socialchoice import Election
 
 example_votes = [[0, 1, "win"], [3, 2, "loss"], [2, 3, "win"], [0, 3, "tie"], [3, 0, "win"]]
 example_candidates = {0, 1, 2, 3}
-
-
-def test_add_votes():
-    e = Election()
-    e.add_votes()
-    assert e.get_votes() == []
-    e.add_votes(example_votes[0])
-    assert e.get_votes() == [[0, 1, "win"]]
-    e.add_votes(*example_votes[1:])
-    assert e.get_votes() == example_votes
-
-
-def test_get_votes():
-    e = Election()
-    assert e.get_votes() == []
-    e.add_votes(*example_votes)
-    assert e.get_votes() == example_votes
-
-    e1 = Election(*(example_votes[1:]))
-    assert e1.get_votes() == example_votes[1:]
+empty_election = Election([])
 
 
 def test_get_candidates():
-    e = Election()
-    assert e.get_candidates() == set()
-    e.add_votes(*example_votes)
-    assert e.get_candidates() == example_candidates
-    e1 = Election(*example_votes)
+    assert empty_election.get_candidates() == set()
+
+    e1 = Election(example_votes)
     assert e1.get_candidates() == example_candidates
 
 
-def test_get_victory_graph():
-    e = Election()
-    assert len(e.get_victory_graph()) == 0
+def test_get_victory_graph_empty():
+    assert len(empty_election.get_victory_graph()) == 0
 
 
 def test_get_matchup_graph():
-    e = Election()
-    assert len(e.get_matchup_graph()) == 0
+    assert len(empty_election.get_matchup_graph()) == 0
 
 
 def test_get_matchups():
-    e_empty = Election()
+    e_empty = Election([])
     assert e_empty.get_matchups() == {}
 
-    e_1 = Election(*example_votes)
+    e_1 = Election(example_votes)
     assert e_1.get_matchups() == {0: {1: {'wins': 1, 'losses': 0, 'ties': 0},
                                       2: {'wins': 0, 'losses': 0, 'ties': 0},
                                       3: {'wins': 0, 'losses': 1, 'ties': 1}},
@@ -63,24 +40,21 @@ def test_get_matchups():
 
 
 def test_get_ranked_pairs_ranking():
-    e_empty = Election()
-    assert e_empty.get_ranked_pairs_ranking() == []
+    assert empty_election.ranking_by_ranked_pairs() == []
 
-    e_1 = Election(*example_votes)
-    assert e_1.get_ranked_pairs_ranking() == [2, 3, 0, 1]
+    e_1 = Election(example_votes)
+    assert e_1.ranking_by_ranked_pairs() == [2, 3, 0, 1]
 
 
 def test_get_win_ratio():
-    e_empty = Election()
-    assert e_empty.get_win_ratio() == []
+    assert empty_election.ranking_by_win_ratio() == []
 
-    e_1 = Election(*example_votes)
-    assert e_1.get_win_ratio() == [(2, 1.0), (0, 0.5), (3, 1 / 3), (1, 0.0)]
+    e_1 = Election(example_votes)
+    assert e_1.ranking_by_win_ratio() == [(2, 1.0), (0, 0.5), (3, 1 / 3), (1, 0.0)]
 
 
 def test_get_win_tie_ratio():
-    e_empty = Election()
-    assert e_empty.get_win_tie_ratio() == []
+    assert empty_election.ranking_by_win_tie_ratio() == []
 
-    e_1 = Election(*example_votes)
-    assert e_1.get_win_tie_ratio() == [(2, 1.0), (0, 2 / 3), (3, 0.5), (1, 0.0)]
+    e_1 = Election(example_votes)
+    assert e_1.ranking_by_win_tie_ratio() == [(2, 1.0), (0, 2 / 3), (3, 0.5), (1, 0.0)]
