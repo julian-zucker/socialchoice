ae407a65752f2274e0390279966b1f97534e4f9b
Julian Zucker <julian.zucker@gmail.com>
2019-05-27 09:27:27 -0400

    Use readme for description

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
 
+
+with open("README.md") as f:
+    readme = f.read()
+
 setup(
     name="socialchoice",
-    version="0.0.3",
+    version="0.0.4",
     packages=["socialchoice", "socialchoice.pairwisecollapse"],
     package_dir={"": "socialchoice"},
     url="https://github.com/julian-zucker/socialchoice",
@@ -10,5 +14,6 @@ setup(
     author="Julian Zucker",
     author_email="julian.zucker@gmail.com",
     description="Social Choice Theory in Python",
+    long_description=readme,
     install_requires=["networkx", "pygraphviz", "pytest", "scipy"],
 )
