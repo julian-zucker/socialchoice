63f7076d9a61ce5b9e39773af61691db1acb02e6
Julian Zucker <julian.zucker@gmail.com>
2019-05-27 09:27:27 -0400

    Use readme for description

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+source venv/bin/activate
+
 python3 setup.py sdist && \
 python3 -m twine upload dist/* --config-file ~/.pypirc -r pypi --verbose  && \
-rm -r build/ dist/
+rm -r dist/
